356096cfe532a4c7e10104207207614a4de5d2d9
yuhyeongchan <gudcks305@gmail.com>
2023-09-23 15:24:22 +0900

    feat : async controller

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,15 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  load_time: Date = new Date();
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getAsync() {
+    return {
+      message: 'I am an async message chage',
+      hot_reload: this.load_time,
+    };
   }
 }
